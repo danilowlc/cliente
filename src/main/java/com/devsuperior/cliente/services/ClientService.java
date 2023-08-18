138426002fd27be6ac69bcca3674b7f9c4f93fdd
Danilo Cunha <danilowlc@gmail.com>
2023-08-17 21:28:41 -0300

    new Client, update, delete

--- src/main/java/com/devsuperior/cliente/services/ClientService.java
+++ src/main/java/com/devsuperior/cliente/services/ClientService.java
@@ -3,11 +3,15 @@ package com.devsuperior.cliente.services;
 import com.devsuperior.cliente.dto.ClientDTO;
 import com.devsuperior.cliente.entities.Client;
 import com.devsuperior.cliente.repositories.ClientRepository;
+import com.devsuperior.cliente.services.exceptions.DatabaseException;
 import com.devsuperior.cliente.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -37,6 +41,33 @@ public class ClientService {
         client = repository.save(client);
         return new ClientDTO(client);
     }
+
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO dto){
+        try {
+            Client client = repository.getReferenceById(id);
+            copyDtoToEntity(dto, client);
+            client = repository.save(client);
+            return new ClientDTO(client);
+        }
+        catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id){
+        if (!repository.existsById(id)){
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+        try {
+            repository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e){
+            throw new DatabaseException("Falha de integridade referencial");
+        }
+    }
+
     private void copyDtoToEntity(ClientDTO dto, Client entity){
         entity.setName(dto.getName());
         entity.setCpf(dto.getCpf());
